fd1b2ad206b071f91a25128ec9d560a8754ad918
Patrice Gagnon <patrice@tenaya.ca>
2018-04-23 12:40:54 -0400

    More on dispatching and routes

--- routes/distance_route.js
+++ routes/distance_route.js
@@ -4,8 +4,8 @@ var async = require('async');
 
 module.exports = function(app, oauth) {
 
+  // Calculate distance with google api to the yard
   app.get('/distance/:postal', [oauth], (req, res) => {
-    // Calculate distance with google map.
     var origin = "7628 Flewellyn Rd Stittsville, ON, K2S1B6";
     // var twoAddress = baseAddress + "K2S1B6|" + baseAddress + req.params.postal
     var destination = req.params.postal + " Canada";
@@ -20,6 +20,23 @@ module.exports = function(app, oauth) {
       res.json(distance);
     });
   });
+
+  app.post('/distancediff', [oauth], (req, res) => {
+    var origin      = req.body.origin;
+    var destination = req.body.destination;
+    var url = "https://maps.googleapis.com/maps/api/distancematrix/json?key=" + process.env.GOOGLE_MAP_TOKEN +
+    '&' + qs.stringify({
+        // origins: twoAddress,
+        // destinations: twoAddress
+        origins: origin,
+        destinations: destination
+    });
+    request.custom("GET", url, {}, {}, distance => {
+      res.json(distance);
+    });
+  });
 }
+
+
 // From the doc
 // https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOUR_API_KEY
--- routes/quotecars_route.js
+++ routes/quotecars_route.js
@@ -14,7 +14,7 @@ const Op = db.Op;
 module.exports = function(app, oauth) {
 
   // Returned all cars.
-  // Can't do later, for performance...
+  // Can't do, for performance...
   app.get('/quotescars', [oauth], (req, res) => {
       cars.findAll({
           include: [{
@@ -33,6 +33,32 @@ module.exports = function(app, oauth) {
           }]
       }).then(lst => {
           res.json(lst);
+      });
+  })
+
+  // Returned 1 car.
+  // Can't do, for performance...
+  app.get('/quotescar/:carNo', [oauth], (req, res) => {
+      cars.findOne({
+          where: {
+              idQuoteCars: req.params.carNo
+          },
+          include: [{
+              model: vehiclesinfo, as: 'information'
+          }, {
+              model: address, as: 'address'
+          }, {
+              model: quote, as: 'quote',
+              include: [{
+                  model: client, as: 'customer'
+              }, {
+                  model: users, as: 'dispatcher'
+              }, {
+                  model: status, as: 'status'
+              }]
+          }]
+      }).then(car => {
+          res.json(car);
       });
   })
 
